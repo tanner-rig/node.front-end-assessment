674fcd18fab9b7a83cd858e5ef2b753f63d0026d
Tanner Rigby <tanner.rigby@payclip.com>
2018-02-22 09:05:18 -0600

    first push

---
+++ README.md
@@ -0,0 +1,24 @@
+# clip-front-end-assessment
+
+This NodeJS application returns an array of objects for interviewers to use for their assessment
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku CLI](https://cli.heroku.com/) installed.
+
+```
+$ git clone git@github.com:tanner-rig/node.front-end-assessment.git # or clone your own fork
+$ cd clip-front-end-assessment
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:3000](http://localhost:3000/).
+
+## Deploying to Heroku
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open // opens app
+```
---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+const sports = [
+  { id: 'ab123aa', name: 'Baseball' },
+  { id: 'ab124aa', name: 'Golf' },
+  { id: 'ab125aa', name: 'Hockey' },
+  { id: 'ab126bb', name: 'Soccer' },
+  { id: 'ab127bb', name: 'Football' },
+  { id: 'ab128cc', name: 'Skiing' },
+  { id: 'ab129dd', name: 'Ping Pong' },
+  { id: 'ab130cc', name: 'Foosball' },
+];
+
+
+// Express
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', function(req, res) {
+  res.send('Working');
+})
+
+app.get('/sports', function(req, res) {
+  res.send(sports);
+})
+
+// Start server
+app.listen(PORT);
+console.log('API is running on port 3000');
